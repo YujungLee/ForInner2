31a22d89c1450c06518348d8b93b4b273dd1aa87
Yujung Lee <yujunglee@yujungui-macbook-pro.local>
2016-06-23 12:48:36 +0900

    Initial Commit

---
+++ StoreSearch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StoreSearch
+//
+//  Created by Yujung Lee on 2016. 6. 23..
+//  Copyright © 2016년 YujungLee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
